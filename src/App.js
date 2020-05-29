a163210d837f671eefd29cbbee1d3a388ae78647
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2020-05-30 01:29:53 +0300

    Community map embedding, changed descriptions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const embedUrl = process.env.REACT_APP_OCM_EMBED_URL
+
+const Splash = () => {
+  return <div className="Splash-header">
+    <img src={logo} className="Splash-logo" alt="logo" />
+    <p>
+      Non-zone loading...
+ </p>
+  </div>
+}
+
+const Map = () => {
+  return <iframe src={embedUrl} title="Open Communty Map" allow="geolocation" style={{ borderWidth: 0 }}
+    width="100%" height="100%" />
+
+}
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => setTimeout(() => setLoading(false), 2000), []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? <Splash /> : <Map />}
     </div>
   );
 }
